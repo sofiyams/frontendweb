58929f8d80935ee5ac7224521ca9d622c2174459
sofiyams <58123538+sofiyams@users.noreply.github.com>
2020-03-03 20:53:23 +0000

    Implementing lab 6 into website and fixing grid

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,87 @@ function toggleNav() {
 }
 closeNav.addEventListener('click', toggleNav);
 openNav.addEventListener('click', toggleNav);
+
+
+//js for qna page
+
+(() => {
+  const listElement = document.getElementById('qna');
+  const newItem = document.getElementById('newItem');
+  const addBtn = document.getElementById('addBtn');
+  const clearBtn = document.getElementById('clearBtn');
+
+  function addItem(item) {
+    if(!item) return;
+    const itemElement = document.createElement('li');
+    itemElement.textContent = item;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'x';
+    itemElement.appendChild(deleteButton);
+    deleteButton.addEventListener('click', ev => {
+      listElement.removeChild(itemElement);
+      save();
+    });
+    listElement.appendChild(itemElement);
+  };
+
+  function addList(list) {
+  list.forEach(addItem);
+  }
+
+  function clearList() {
+    while(listElement.firstChild) {
+      listElement.removeChild(listElement.firstChild);
+    }
+  }
+
+  function save() {
+    const items = [...listElement.childNodes];
+    if(items.length) {
+      const list = items.map(item => {
+        return item.textContent.slice(0, -1);
+      });
+      localStorage.setItem('qna-list', list);
+    } else {
+      localStorage.removeItem('qna-list');
+    }
+  }
+
+  function load() {
+    const qnaList = localStorage.getItem('qna-list');
+    if(qnaList) {
+      addList(qnaList.split(','));
+    }
+  }
+
+  addBtn.addEventListener('click', ev => {
+    addList(newItem.value.split(','));
+    newItem.value = null;
+    save();
+  });
+
+  newItem.addEventListener("keyup", ev => {
+    if (ev.key === "Enter") {
+      addBtn.click();
+    }
+  });
+
+  clearBtn.addEventListener('click', ev => {
+    clearList();
+    save();
+  });
+
+  // Saving data for later use
+  window.addEventListener('beforeunload', save);
+
+  // Loading data from local storage
+  window.addEventListener('DOMContentLoaded', load);
+
+  window.addEventListener('storage', ev => {
+    if(ev.key == "qna-list") {
+      clearList();
+      load();
+    }
+  })
+
+})();
