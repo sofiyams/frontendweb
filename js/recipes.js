22edf1e08e9d74b6adba51f077c2dcdbf79cc171
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-03-11 10:05:24 +0000

    changing buttons and styles

--- js/recipes.js
+++ js/recipes.js
@@ -12,9 +12,9 @@ function buildCake(cake) {
   const image = document.createElement('img');
   image.src = cake.img;
   article.appendChild(image);
-  const ckbtn = document.createElement('ckbtn');
-  ckbtn.textContent = cake.ckbtn;
-  article.appendChild(ckbtn);
+  const btn = document.createElement('btn');
+  btn.textContent = cake.btn;
+  article.appendChild(btn);
   return article;
 }
 
@@ -23,7 +23,7 @@ const cakeData = [
     name: "Chocolate Cake",
     img: "../2019-20-sofiyams/img/chocolate-cake.jpg",
     description: "Gooey chocolate cake",
-    button:"ckbtn"
+    button:"btn"
   },
   {
     name: "Chocolate Drip Cake",
@@ -34,8 +34,22 @@ const cakeData = [
     name: "Victoria Sponge Cake",
     img: "../2019-20-sofiyams/img/VSCake.jpg",
     description: "Airy sponge cake"
-  }
-
+  },
+  {
+    name: "Pretty Little Liars Cake",
+    img: "../2019-20-sofiyams/img/PLL.jpg",
+    description: "Pretty little liars vanilla sponge cake"
+  },
+  {
+    name: "Fresh Cream Fruit Cake",
+    img: "../2019-20-sofiyams/img/fruit_cake.jpg",
+    description: "A fresh cream sponge cake with fresh fruit"
+  },
+  {
+    name: "Fondant Love Sponge Cake",
+    img: "../2019-20-sofiyams/img/Aishteru.jpg",
+    description: "Small sponge cake with fondant icing"
+  },
 ]
 
 cakeData.forEach(loadCake);
