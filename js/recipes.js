bb835f291d307dbf339e4ee0382b5ea64762a121
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-03-17 11:51:20 +0000

    changing nav to condense code

--- js/recipes.js
+++ js/recipes.js
@@ -63,11 +63,11 @@ document.getElementById('cakes').appendChild(cake);
 
 document.addEventListener('click', function (e) {
     e = e || window.event;
-    var target = e.target || e.srcElement;
+    const target = e.target || e.srcElement;
 
     if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
         if (target.hasAttribute('data-target')) {
-            var m_ID = target.getAttribute('data-target');
+            const m_ID = target.getAttribute('data-target');
             document.getElementById(m_ID).classList.add('open');
             e.preventDefault();
         }
@@ -75,7 +75,7 @@ document.addEventListener('click', function (e) {
 
     // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
-        var modal = document.querySelector('[class="modal open"]');
+        const modal = document.querySelector('[class="modal open"]');
         modal.classList.remove('open');
         e.preventDefault();
     }
