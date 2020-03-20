3a133f644deb897c1f99d7c6960d20f14467618a
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-03-20 15:17:15 +0000

    using console log to bring recipe through

--- js/more_recipes.js
+++ js/more_recipes.js
@@ -2,6 +2,7 @@
 
 const API_URL = `https://api.edamam.com/search?q=cake&app_id=3d9c5788&app_key=ec37d78e5c7a3a311ad52de140e9b815`;
 // const API_URL = `https://api.edamam.com/recipe/cake`;
+
 const cakeLabels = [
                   "Caramel Cake",
                   "Cakespy: Crumb Cake Shake Recipe",
@@ -19,15 +20,16 @@ const getRecipeAsync = async (cakes) => {
   return data;
 }
 
-const showRecipes = (morecake) => {
+const showRecipe = (recipe) => {
+  console.log(recipe);
   const content =
    `<section>
-      <img src="${morecake.hits.recipe.image}" alt="recipe.image">
+      <img src="${recipe.image}" alt="recipe.image">
       <div>
         <ul>
-          <li>Name: ${morecake.hits.recipe.label}</li>
-          <li>Ingredients: ${morecake.hits.recipe.ingredientsLines}</li>
-          <li>Extra Info: ${morecake.hits.recipe.healthLabels}</li>
+          <li>Name: ${recipe.label}</li>
+          <li>Ingredients: ${recipe.hits.recipe.ingredientsLines}</li>
+          <li>Extra Info: ${recipe.hits.recipe.healthLabels}</li>
         </ul>
       </div>
     </section>`;
@@ -36,10 +38,14 @@ const showRecipes = (morecake) => {
 
 showData.innerText = "";
 
-cakeLabels.forEach( cake => {
-getRecipeAsync(cake)
-  .then(data => {
-    console.log(data);
-    showRecipes(data);
-  })
-});
+
+const loadCakes = data => {
+  showRecipe(data.hits[0]);
+}
+getRecipeAsync().then(loadCakes);
+// cakeLabels.forEach( cake => {
+//   .then(data => {
+//     console.log(data);
+//     showRecipes(data);
+//   })
+// });
