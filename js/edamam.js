921e4e88cb55dbf60edc58145be8b7a84ddd4e21
Cinema System <sofiya_abdullatif@hotmail.co.uk>
2020-03-27 16:05:12 +0000

    made a loop for ingredients lines and cleaned the .js file

--- js/edamam.js
+++ js/edamam.js
@@ -14,10 +14,13 @@ function buildRecipe(data) {
   h4.textContent = recipe.label;
   article.appendChild(h4);
   const ul = document.createElement('ul');
+  recipe.ingredientLines.forEach((ingredient) => {
+    let li = document.createElement('li');
+    li.textContent = ingredient;
+    ul.appendChild(li);
+  });
+
   let li = document.createElement('li');
-  li.textContent = recipe.ingredientLines;
-  ul.appendChild(li);
-  li = document.createElement('li');
   li.textContent = recipe.healthLabels;
   ul.appendChild(li);
   article.appendChild(ul);
@@ -36,73 +39,8 @@ const getRecipeAsync = async (cakes) => {
 }
 
 getRecipeAsync();
-
-
-
-
-
-// const cakeLabels = [
-//   {
-//     img: "https://www.edamam.com/web-img/482/482417e9943411f0e7db4be74a7b5114.jpg",
-//     label: "Caramel Cake",
-//     ingredientsLines: {
-//                         "2 cup sifted cake flour(not self-rising; sift before measuring),for cake",
-//                         "2 tbsp sifted cake flour,for cake",
-//                         "1 tsp Baking Powder,for cake",
-//                         "3/4 tsp Baking Soda,for cake",
-//                         "1/2 tsp Salt,for cake",
-//                         "1 stick unsalted butter, softened(4 oz),for cake",
-//                         "1 cup Granulated Sugar,for cake",
-//                         "1 tsp pure vanilla extract,for cake",
-//                         "2 x large eggs, at room temperature 30 minutes,for cake",
-//                         "1 cup Buttermilk,well shaken,for cake",
-//                         "1 cup Heavy Cream,for caramel glaze",
-//                         "1/2 cup packed light brown sugar,for caramel glaze",
-//                         "1 tbsp Light Corn Syrup,for caramel glaze",
-//                         "1 tsp pure vanilla extract,for caramel glaze"
-//                       },
-//     healthLabels: {
-//                     "Vegetarian",
-//                     "Peanut-Free",
-//                     "Tree-Nut-Free",
-//                     "Alcohol-Free"
-//                   },
-//   }
-// ]
-                  // "Cakespy: Crumb Cake Shake Recipe",
-                  // "Lollipop Garden Cake",
-                  // "Champagne Cake",
-                  // "Sprinkle Cake Bites",
-                  // "Cake pops",
-                  // "project wedding cake: the cake is baked",
-                  // "Lemon or Orange Cake (Cake Citron ou Cake"
-
-
-// const showRecipe = (recipe) => {
-//   const content =
-//    `<section>
-//       <img src="${recipe.image}" alt="recipe.image">
-//       <div>
-//         <ul>
-//           <li>Name: ${recipe.label}</li>
-//           <li>Ingredients: ${recipe.ingredientsLines}</li>
-//           <li>Extra Info: ${recipe.healthLabels}</li>
-//         </ul>
-//       </div>
-//     </section>`;
-//   showData.insertAdjacentHTML("beforeend", content);
-// }
-//
-// showData.innerText = "";
-
-
-// const loadCakes = data => {
-//   showRecipe(data.hits);
-// }
-// getRecipeAsync().then(loadCakes);
-// cakeLabels.forEach( cake => {
-//   .then(data => {
-//     console.log(data);
-//     showRecipes(data);
-//   })
-// });
+// add in a section like recipe page
+// make healthLabels into span and loop it like ul
+// will have to change the const main to get the section and not the main perhaps?
+// style it the same way as recipes with cake grid (section) <-- put the display flex in here
+// then style recipe(article)
